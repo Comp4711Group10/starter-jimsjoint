bb06f4dd1ff4a6ea9d8c89332d63348072601f58
mebighobo <john_chang69@hotmail.com>
2016-02-21 00:58:56 -0600

    Fixing some Bug and fixed flush() and detais() method.

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -35,12 +35,10 @@ class Order extends Application {
         $this->data['pagebody'] = 'show_menu';
         $this->data['order_num'] = $order_num;
         //FIXME
-        $this->data['title'] = 'Order  #'.$order_num 
-                . ' (' . number_format($this->orders->total($order_num), 2) 
-                . ')';
-        
-        
-
+	$this->data['title'] = "Order # ".$order_num
+                .' ('
+                .number_format($this->orders->total($order_num),2)
+                .')';
         // Make the columns
         $this->data['meals'] = $this->make_column('m');
         $this->data['drinks'] = $this->make_column('d');
@@ -91,16 +89,15 @@ class Order extends Application {
         $this->data['order_num'] = $order_num;
         //FIXME
         
-        $this->data['total'] = number_format($this->orders->total($order_num), 2);
-
-        $items = $this->orderitems->group($order_num);
-        foreach($items as $item)
-        {
-                $menuitem = $this->menu->get($item->item);
-                $item->code = $menuitem->name;
-        }
-        $this->data['items'] = $items;
-        
+	$this->data['total'] = number_format($this->orders->total($order_num), 2);
+        $items = $this->orders->details($order_num);
+	foreach ($items as $item)
+	{
+		$menuitem = $this->menu->get($item->item);
+		$item->code = $menuitem->name;
+	}
+	$this->data['items'] = $items;
+	$this->data['okornot'] = $this->orders->validate($order_num) ? "" : "disabled";
         $this->render();
     }
 
